d3e66d472f89d01bcd538d79cd4bbff002708fff
filauro1989 <filauro1989@gmail.com>
2021-12-22 12:16:07 +0100

    search on chats

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const wpmain = new Vue ({
         // assegno indexContact per avere un indice da utilizzare al click
         indexContact: 0,
         text: '',
+        searchText: '',
         contacts: [
             {
             name: "Michele",
@@ -128,6 +129,15 @@ const wpmain = new Vue ({
                     status: "received",
                 });
             }, 1000);
+        },
+        searchChat: function() {
+            this.contacts.forEach(contact => {
+                if (contact.name.toLowerCase().includes(this.searchText.toLowerCase())){
+                    contact.visible = true;
+                } else {
+                    contact.visible = false;
+                }
+            });
         }
     },
 });
@@ -135,4 +145,7 @@ const wpmain = new Vue ({
 // Milestone 3
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 // Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+// Milestone 4
+// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 
